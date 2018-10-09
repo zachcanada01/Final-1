65d526cac8325c63eac88139a4e83cd3d45ba737
Cheyenne Jones <cj1011012020@gmail.com>
2018-10-09 18:12:53 -0500

    added code

--- Tic-Tac-Toe/src/project/Main.java
+++ Tic-Tac-Toe/src/project/Main.java
@@ -1,11 +1,48 @@
 package project;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			TicTacToe.displayMap();
-			WinsAndLoses.displayWAL();
+		boolean playing = true;
+		boolean x = true;
+		
+		Scanner input = new Scanner(System.in);
+	//	WinsAndLoses.displayWAL();
+		TicTacToe tictoe = new TicTacToe();
+		
+		tictoe.displayTemplate();
+		
+		
+		while (playing) {
+			if (x) {
+				System.out.println("Player X Input a Number");
+				tictoe.placePiece("X", input.nextInt());
+				
+				tictoe.displayMap();
+				
+				x = !x;
+				
+				System.out.println("Type OKAY to continue >>");
+				input.next();
+			} else {
+				System.out.println("Player O Input a Number");
+				tictoe.placePiece("O", input.nextInt());
+				
+				tictoe.displayMap();
+				
+				x = !x;
+				
+				System.out.println("Type OKAY  to continue >>");
+				input.next();
+			}
+			
+			
+			if (tictoe.win())
+				System.out.println((x ? "o" : "x") + " Winner");
+		}
 		
 	}
 
--- Tic-Tac-Toe/src/project/TicTacToe.java
+++ Tic-Tac-Toe/src/project/TicTacToe.java
@@ -1,9 +1,51 @@
 package project;
 
 public class TicTacToe {
+	String[] board = {
+			"0","1","2",
+			"3","4","5",
+			"6","7","8"
+	};
 
-	public static void displayMap() {
+	public void displayTemplate() {
+		System.out.println("" 
+		+ "\n0|1|2"
+		+ "\n-----"
+		+ "\n3|4|5"
+		+ "\n-----"
+		+ "\n6|7|8");
+	}
+	
+	public void displayMap() {
+		System.out.println("" 
+				+ "\n" + board[0] + "|" + board[1] + "|" + board[2] + ""
+				+ "\n-----"
+				+ "\n" + board[3] + "|" + board[4] + "|" + board[5] + ""
+				+ "\n-----"
+				+ "\n" + board[6] + "|" + board[7] + "|" + board[8] + "");
+	}
+	
+	public void placePiece(String piece, int position) {
+		board[position] = piece;
+	}
+	
+	public boolean win() {
+		int x = 0;
+		int o = 0;
+		for (String piece : board) {
+			if (piece.equalsIgnoreCase("x"))
+				x++;
+			else if (piece.equalsIgnoreCase("o"))
+				o++;
+		}
 		
+		if (x >= 3 || o >= 3)
+			return checkWin();
 		
+		return false;
+	}
+	
+	public boolean checkWin() {
+		return true;
 	}
 }
