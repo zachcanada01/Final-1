b6492bc333b7fd6b93fd7faf9fe52d57145f7152
Zach <zachcanada23@gmail.com>
2018-10-10 09:48:49 -0500

    TicTacToe

---
+++ Final/src/main/WinsLosses.java
@@ -0,0 +1,22 @@
+package main;
+
+import javax.swing.JOptionPane;
+
+public class WinsLosses {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int X;
+		int O;
+		int win;
+		int lose;
+		
+		if(x =   ) {
+			
+		}
+		
+		
+		
+	}
+
+}
